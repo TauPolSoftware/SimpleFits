166b10e9b0b2ff7bc5299b54fb877c74e1f97112
inugent <>
2013-02-08 14:52:15 +0000

    Patch of TauA1NuContraintFitter

--- FitSoftware/src/TauA1NuConstrainedFitter.cc
+++ FitSoftware/src/TauA1NuConstrainedFitter.cc
@@ -23,25 +23,33 @@ TauA1NuConstrainedFitter::TauA1NuConstrainedFitter(unsigned int ambiguity_,std::
   for(int i=0; i<LorentzVectorParticle::NVertex;i++){
     for(int j=0; j<LorentzVectorParticle::NVertex;j++)incov(i,j)=VertexCov(i,j);
   }
+  std::cout << "TauA1NuConstrainedFitter::TauA1NuConstrainedFitter A" << std::endl;
   bool hasa1(false),hasnu(false);
   for(unsigned int p=0;p<particles.size();p++){
     int offset=LorentzVectorParticle::NVertex;
-    if(particles.at(p).Charge()<0.1){hasnu=true; offset+=LorentzVectorParticle::NLorentzandVertexPar;}
+      if(fabs(particles.at(p).Charge())<=0.1){hasnu=true; offset+=LorentzVectorParticle::NLorentzandVertexPar;}
     else{hasa1=true;}
     for(int i=0; i<LorentzVectorParticle::NLorentzandVertexPar;i++){
       inpar(i+offset,0)=particles.at(p).Parameter(i);
       for(int j=0; j<LorentzVectorParticle::NLorentzandVertexPar;j++)incov(i+offset,j+offset)=particles.at(p).Covariance(i,j);
     }
   }
-  if(!(hasa1 && hasnu)) return;
+  std::cout << "TauA1NuConstrainedFitter::TauA1NuConstrainedFitter B" << std::endl;
+  if(!hasa1 || !hasnu) return;
   // store expanded par for computation of final par (assumes fit has neglegible impact on a1 correlations with vertex errors)
+  std::cout << "TauA1NuConstrainedFitter::TauA1NuConstrainedFitter C1" << std::endl;
   exppar.ResizeTo(nexpandedpar,1);
+  std::cout << "TauA1NuConstrainedFitter::TauA1NuConstrainedFitter C2" << std::endl;
   exppar=ComputeInitalPar(inpar);
+  std::cout << "TauA1NuConstrainedFitter::TauA1NuConstrainedFitter C3" << std::endl;
   expcov.ResizeTo(nexpandedpar,nexpandedpar);
+  std::cout << "TauA1NuConstrainedFitter::TauA1NuConstrainedFitter C4" << std::endl;
   expcov=ErrorMatrixPropagator::PropogateError(&TauA1NuConstrainedFitter::ComputeInitalPar,inpar,incov);
   // store linearization point
+  std::cout << "TauA1NuConstrainedFitter::TauA1NuConstrainedFitter C5" << std::endl;
   par_0.ResizeTo(npar);
   cov_0.ResizeTo(npar,npar);
+  std::cout << "TauA1NuConstrainedFitter::TauA1NuConstrainedFitter c" << std::endl;
   for(int i=0; i<npar;i++){
     par_0(i)=exppar(i,0);
     for(int j=0;j<npar;j++){cov_0(i,j)=expcov(i,j);}
