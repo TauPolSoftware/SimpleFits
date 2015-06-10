b1209417350df9d97e33e878a8c695f46a6237bb
bkargoll <bastian.kargoll@cern.ch>
2015-06-10 13:54:16 +0200

    LorentzVectorParticle now returns Covariance matrix in (px, py, pz, E) convention

--- FitSoftware/src/LorentzVectorParticle.cc
+++ FitSoftware/src/LorentzVectorParticle.cc
@@ -31,3 +31,26 @@ TString LorentzVectorParticle::Name(int i){
   if(i==vz)  return "vz";
   return "invalid";
 }
+
+double LorentzVectorParticle::Parameter(int i){
+  if(i==E)  return sqrt(pow(Particle::Parameter(m),2.0)+pow(Particle::Parameter(px),2.0)+pow(Particle::Parameter(py),2.0)+pow(Particle::Parameter(pz),2.0));
+  if(i==p)  return sqrt(pow(Particle::Parameter(px),2.0)+pow(Particle::Parameter(py),2.0)+pow(Particle::Parameter(pz),2.0));
+  if(i==pt) return sqrt(pow(Particle::Parameter(px),2.0)+pow(Particle::Parameter(py),2.0));
+  return Particle::Parameter(i);
+}
+
+double LorentzVectorParticle::Covariance(int i,int j){
+	if(i==E && j==E)
+		return pow(Parameter(E), -2) * (
+				pow(Parameter(px),2)*Particle::Covariance(px,px) + pow(Parameter(py),2)*Particle::Covariance(py,py) + pow(Parameter(pz),2)*Particle::Covariance(pz,pz) + Parameter(m)*Parameter(m)* Particle::Covariance(m,m) +
+				2*Parameter(px)*Parameter(py)*Particle::Covariance(px,py) + 2*Parameter(px)*Parameter(pz)*Particle::Covariance(px,pz) + 2*Parameter(m)*Parameter(px)*Particle::Covariance(px,m) +
+				2*Parameter(py)*Parameter(pz)*Particle::Covariance(py,pz) + 2*Parameter(m)*Parameter(py)*Particle::Covariance(py,m) +
+				2*Parameter(m)*Parameter(pz)*Particle::Covariance(pz,m));
+	if( i==E && (j==px || j==py || j==pz) )
+		return -(Particle::Covariance(j,m)*Parameter(m) + Particle::Covariance(j,px)*Parameter(px) +
+				 Particle::Covariance(j,py)*Parameter(py) + Particle::Covariance(j,pz)*Parameter(pz)) / Parameter(E);
+	if (j==E && (i==px || i==py || i==pz))
+		return Covariance(j,i);
+
+	return Particle::Covariance(i,j);
+}
