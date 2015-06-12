dd7cd40f281e6c1a33864b41f7fa93af1735a08d
Alexander Zotz <zotz@physik.rwth-aachen.de>
2015-06-12 17:58:55 +0200

    new class PTObject, that saves a 2x1 parameter matrix and a 2x2 covariance matrix and has a small list of helperfunctions

---
+++ FitSoftware/src/PTObject.cc
@@ -0,0 +1,30 @@
+/*
+ * PTObject.cc
+ *
+ *  Created on: Jun 10, 2015
+ *      Author: zotz
+ */
+#include "SimpleFits/FitSoftware/interface/PTObject.h"
+
+  PTObject::PTObject(){
+	TMatrixT<double> par(2,1);
+	par_.ResizeTo(par);
+	par_=par;
+	TMatrixTSym<double> cov(2,2);
+	cov_.ResizeTo(cov);
+	cov_=cov;
+  }
+
+  PTObject::PTObject(const PTObject& other){
+	par_.ResizeTo(other.Par());
+	par_ = other.Par();
+	cov_.ResizeTo(other.Cov());
+	cov_ = other.Cov();
+  }
+
+  PTObject::PTObject(TMatrixT<double> par, TMatrixTSym<double> cov){
+	par_.ResizeTo(par);
+	par_=par;
+	cov_.ResizeTo(cov);
+	cov_=cov;
+  }
