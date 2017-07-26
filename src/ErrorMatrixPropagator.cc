073925fce53f98d95363aa0c6398e70b5142345b
Thomas Mueller <t.muller@cern.ch>
2017-07-27 00:14:42 +0200

    ErrorMatrixPropagator: more standard way of calculation: newCov = jacobian * oldCov * jacobian^T, no change of results

--- src/ErrorMatrixPropagator.cc
+++ src/ErrorMatrixPropagator.cc
@@ -5,29 +5,33 @@
 TMatrixTSym<double> ErrorMatrixPropagator::PropagateError(TMatrixT<double> (*function)(TMatrixT<double> &par),
                                                           TMatrixT<double> inPar, TMatrixTSym<double> inCov,
                                                           double epsilon, double errorEpsilonRatio) {
+  
+  // https://de.wikipedia.org/wiki/Fehlerfortpflanzung#Generalisiertes_Fehlerfortpflanzungsgesetz
+  // https://de.wikipedia.org/wiki/Jacobi-Matrix
+  // https://root.cern.ch/doc/v608/classTMatrixTSym.html#ab26c079dab1c5e71c49934fbe4bd6c21
   TMatrixT<double> outPar = function(inPar);
-  TMatrixT<double> jacobian(inPar.GetNrows(), outPar.GetNrows());
-  for (int row = 0; row < inPar.GetNrows(); row++) {
+  TMatrixT<double> jacobian(outPar.GetNrows(), inPar.GetNrows());
+  for (int col = 0; col < jacobian.GetNcols(); col++) {
     TMatrixT<double> inParPlusEpsilon = inPar;
-    double error = std::sqrt(std::fabs(inCov(row, row)));
+    double error = std::sqrt(std::fabs(inCov(col, col)));
     double delta = epsilon;
     if ((std::fabs(delta * errorEpsilonRatio) > error) && (error > 0.0)) {
       delta = std::fabs(error / errorEpsilonRatio);
     }
-    inParPlusEpsilon(row, 0) += delta;
+    inParPlusEpsilon(col, 0) += delta;
     TMatrixT<double> outParPlusEpsilon = function(inParPlusEpsilon);
-    for(int col = 0; col < outPar.GetNrows(); col++) {
-      jacobian(row, col) = (outParPlusEpsilon(col, 0) - outPar(col, 0)) / delta; // Newtons approx.
+    for(int row = 0; row < outPar.GetNrows(); row++) {
+      jacobian(row, col) = (outParPlusEpsilon(row, 0) - outPar(row, 0)) / delta; // Newtons approx.
     }
   }
-  TMatrixTSym<double> outCov = inCov.SimilarityT(jacobian);
+  TMatrixTSym<double> outCov = inCov.Similarity(jacobian);
   /// debug
   /*
-  std::cout << "ErrorMatrixPropagator::PropogateError inCov:" << std::endl;
+  std::cout << "ErrorMatrixPropagator::PropogateError inCov = (" << inCov.GetNrows() << " x " << inCov.GetNcols() << ")" << std::endl;
   inCov.Print();
-  std::cout << "ErrorMatrixPropagator::PropogateError jacobian:" << std::endl;
+  std::cout << "ErrorMatrixPropagator::PropogateError jacobian = (" << jacobian.GetNrows() << " x " << jacobian.GetNcols() << ")" << std::endl;
   jacobian.Print();
-  std::cout << "ErrorMatrixPropagator::PropogateError outCov:" << std::endl;
+  std::cout << "ErrorMatrixPropagator::PropogateError outCov = jacobian * inCov * jacobian^T = (" << outCov.GetNrows() << " x " << outCov.GetNcols() << ")" << std::endl;
   outCov.Print();
   */
   return outCov;
