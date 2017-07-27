3c7e1e986fd880cad58dc5775473ebb3f1449038
Thomas Mueller <t.muller@cern.ch>
2017-07-27 17:04:03 +0200

    Particle.h: improve readability

--- interface/Particle.h
+++ interface/Particle.h
@@ -8,27 +8,40 @@
 // Store B in units of 1/GeV
 
 class Particle {
- public:
+public:
   Particle(TMatrixT<double> par_, TMatrixTSym<double> cov_, int pdgid_, double charge_, double b_);
   virtual ~Particle(){};
 
-  virtual double Parameter(int i) {if(0<=i && i<par.GetNrows()) return par(i,0); return 0;}
-  virtual double Covariance(int i,int j) {if(0<=i && i<cov.GetNrows() && 0<=j && j<cov.GetNrows()) return cov(i,j); return 0;}
-  double BField() const{return b;}
-  int    PDGID() const{return pdgid;}
-  double Charge() const{return charge;}
-  double qB() const{return b*charge;}
-  virtual int    NParameters()=0;
+  virtual double Parameter(int i) {
+    if (0<=i && i<par.GetNrows()) {
+      return par(i, 0);
+    }
+    return 0;
+  }
+  
+  virtual double Covariance(int i, int j) {
+    if (0<=i && i<cov.GetNrows() && 0<=j && j<cov.GetNrows()) {
+      return cov(i, j);
+    }
+    return 0;
+  }
+  
+  double BField() const { return b; }
+  int PDGID() const { return pdgid; }
+  double Charge() const { return charge; }
+  double qB() const { return b * charge; }
+  
+  virtual int NParameters() = 0;
 
-  TMatrixTSym<double> getCovMatrix() const{
-	return cov;
+  TMatrixTSym<double> getCovMatrix() const {
+	  return cov;
   }
 
-  TMatrixT<double> getParMatrix() const{
-	return par;
+  TMatrixT<double> getParMatrix() const {
+	  return par;
   }
 
- private:
+private:
   TMatrixT<double> par;
   TMatrixTSym<double> cov;
   int pdgid;
@@ -37,5 +50,4 @@ class Particle {
 
 };
 #endif
-
 
