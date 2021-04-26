4f48331c341f546969579a9ea3c20b154e653cde
Alexander Zotz <zotz@physik.rwth-aachen.de>
2021-04-26 17:35:03 +0200

    add ChiSquareFunctionUpdator classes for LagrangeMultipliersFitter
    and DiTauConstrainedFitter to be able to use Minuit

--- interface/ChiSquareFunctionUpdator.h
+++ interface/ChiSquareFunctionUpdator.h
@@ -4,12 +4,15 @@
 #include "Minuit2/FCNBase.h"
 #include "TMatrixT.h"
 #include "TauPolSoftware/SimpleFits/interface/TrackHelixVertexFitter.h"
+#include "TauPolSoftware/SimpleFits/interface/LagrangeMultipliersFitter.h"
+#include "TauPolSoftware/SimpleFits/interface/DiTauConstrainedFitter.h"
+#include "TauPolSoftware/SimpleFits/interface/Logger.h"
 
 class ChiSquareFunctionUpdator  : public ROOT::Minuit2::FCNBase {
  public:
   ChiSquareFunctionUpdator(TrackHelixVertexFitter *VF_){VF=VF_;}
   virtual ~ChiSquareFunctionUpdator(){};
-  
+
   virtual double operator() (const std::vector<double> & x)const{
     TMatrixT<double> X(x.size(),1);
     for(unsigned int i=0; i<x.size();i++){X(i,0)=x.at(i);}
@@ -19,7 +22,45 @@ class ChiSquareFunctionUpdator  : public ROOT::Minuit2::FCNBase {
 
  private:
   TrackHelixVertexFitter *VF;
-  
+
+};
+
+class LagrangeMultipliersFitterChiSquareFunctionUpdator  : public ROOT::Minuit2::FCNBase {
+ public:
+  LagrangeMultipliersFitterChiSquareFunctionUpdator(LagrangeMultipliersFitter *LMF_){LMF=LMF_;}
+  virtual ~LagrangeMultipliersFitterChiSquareFunctionUpdator(){};
+
+  virtual double operator() (const std::vector<double> & x)const{
+    int offset = LMF->NPara();
+    TVectorD a(LMF->NPara()),b(LMF->NParb());
+    for(int i=0; i<LMF->NPara(); i++) a(i)=x.at(i);
+    for(int i=0; i<LMF->NParb(); i++) b(i)=x.at(i+offset);
+    return LMF->UpdateChisquare(a,b);
+  }
+  virtual double Up()const{return 1.0;}// Error definiton for Chi^2
+
+ private:
+  LagrangeMultipliersFitter *LMF;
+
+};
+
+class DiTauConstrainedFitterChiSquareFunctionUpdator  : public ROOT::Minuit2::FCNBase {
+ public:
+  DiTauConstrainedFitterChiSquareFunctionUpdator(DiTauConstrainedFitter *DTCF_){DTCF=DTCF_;}
+  virtual ~DiTauConstrainedFitterChiSquareFunctionUpdator(){};
+
+  virtual double operator() (const std::vector<double> & x)const{
+    int offset = DTCF->NPara();
+    TVectorD a(DTCF->NPara()),b(DTCF->NParb());
+    for(int i=0; i<DTCF->NPara(); i++) a(i)=x.at(i);
+    for(int i=0; i<DTCF->NParb(); i++) b(i)=x.at(i+offset);
+    return DTCF->UpdateChisquare(a,b);
+  }
+  virtual double Up()const{return 1.0;}// Error definiton for Chi^2
+
+ private:
+  DiTauConstrainedFitter *DTCF;
+
 };
 #endif
 
